ed670b386ea678e5aee2146260f2849a4cdef6e5
vararais <varadigmaisnann@gmail.com>
2026-01-16 23:51:50 +0700

    Perbaikan error Auth Repository

--- app/src/main/java/com/example/savitapp/repository/AuthRepository.kt
+++ app/src/main/java/com/example/savitapp/repository/AuthRepository.kt
@@ -1,15 +1,25 @@
 package com.example.savitapp.repository
 
-import com.example.savitapp.model.*
+import com.example.savitapp.model.LoginRequest
+import com.example.savitapp.model.LoginResponse
+import com.example.savitapp.model.RegisterRequest
+import com.example.savitapp.model.RegisterResponse
 import com.example.savitapp.service.ApiService
 import retrofit2.Response
 
+// Interface
 interface AuthRepository {
-    suspend fun login(request: LoginRequest): Response<LoginResponse>
     suspend fun register(request: RegisterRequest): Response<RegisterResponse>
+    suspend fun login(request: LoginRequest): Response<LoginResponse>
 }
 
+// Class Implementasi (NetworkAuthRepository) <-- Ini yang dibutuhkan SavitApp
 class NetworkAuthRepository(private val apiService: ApiService) : AuthRepository {
-    override suspend fun login(request: LoginRequest) = apiService.login(request)
-    override suspend fun register(request: RegisterRequest) = apiService.register(request)
+    override suspend fun register(request: RegisterRequest): Response<RegisterResponse> {
+        return apiService.register(request)
+    }
+
+    override suspend fun login(request: LoginRequest): Response<LoginResponse> {
+        return apiService.login(request)
+    }
 }
