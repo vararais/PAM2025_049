775a2065d6d1f5c6d700ea0c28154c7244b6eb95
vararais <varadigmaisnann@gmail.com>
2026-01-16 23:54:54 +0700

    Revisi Savit App

--- app/src/main/java/com/example/savitapp/SavitApp.kt
+++ app/src/main/java/com/example/savitapp/SavitApp.kt
@@ -5,41 +5,28 @@ import com.example.savitapp.repository.AuthRepository
 import com.example.savitapp.repository.NetworkAuthRepository
 import com.example.savitapp.repository.NetworkStuffRepository
 import com.example.savitapp.repository.StuffRepository
-import com.example.savitapp.service.ApiService
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
-import kotlin.getValue
+import com.example.savitapp.service.ApiConfig
 
-// Mirip ContainerApp di repo referensi
 interface AppContainer {
     val authRepository: AuthRepository
     val stuffRepository: StuffRepository
 }
 
 class DefaultAppContainer : AppContainer {
-    // GANTI IP INI SESUAI IP KOMPUTER (Jika pakai HP Asli) atau 10.0.2.2 (Emulator)
-    private val baseUrl = "http://10.0.2.2:3000/api/"
-
-    private val retrofit: Retrofit = Retrofit.Builder()
-        .addConverterFactory(GsonConverterFactory.create())
-        .baseUrl(baseUrl)
-        .build()
-
-    private val retrofitService: ApiService by lazy {
-        retrofit.create(ApiService::class.java)
-    }
+    private val apiService = ApiConfig.getApiService()
 
     override val authRepository: AuthRepository by lazy {
-        NetworkAuthRepository(retrofitService)
+        NetworkAuthRepository(apiService)
     }
 
     override val stuffRepository: StuffRepository by lazy {
-        NetworkStuffRepository(retrofitService)
+        NetworkStuffRepository(apiService)
     }
 }
 
 class SavitApplication : Application() {
     lateinit var container: AppContainer
+
     override fun onCreate() {
         super.onCreate()
         container = DefaultAppContainer()
